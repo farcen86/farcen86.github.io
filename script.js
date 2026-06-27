08a5ad943c748df784d63c89bb43aa9fc20e019a
farcen86 <farcen86@gmail.com>
2026-06-27 14:34:57 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,51 +1,47 @@
-const menunav = document.querySelector('.menu-nav');
+/* ==========================
+   MENU MOBILE
+========================== */
 
-const menunv = [
-    'Hospitalar', 
-    'Descontos e Beneficios', 
-    'Vitaminas', 'Perfumes', 
-    'Linha Infantil'
-  ];
-const renderMenu = () => {
-  const menuHTML = menunv.map((menu)=> {
-    return `<li>${menu}</li>`
-  });
+const menuToggle =
+document.getElementById("menuToggle");
 
-  console.log(menuHTML);
-  menunav.innerHTML = menuHTML.join(' ');
-
-}
-
-renderMenu();
-
-/*configuração de passagem automatica do slide*/
-
-let count = 1;
-document.getElementById("radio1").checked = true;
-
-/*chama a função que criei logo abaixo e determina 5 mil milesegundo para ele ativar*/
-
-setInterval(function(){
-  nextimage();
-}, 5000);
-
-/*contador para passagem da posição slide*/
-
-function nextimage(){
-  count++;
-  if(count>4){
-    count = 1;
-  }
-
-/*faz a checagem e adiciona 1 na posicação slide */
-
-  document.getElementById("radio"+count).checked = true;
-
-}
-
-const menuToggle = document.getElementById("menuToggle");
-const menuNav = document.getElementById("menuNav");
+const menuNav =
+document.getElementById("menuNav");
 
 menuToggle.addEventListener("click", () => {
-    menuNav.classList.toggle("active");
+
+  menuNav.classList.toggle("active");
+
 });
+
+/* ==========================
+   SLIDER AUTOMÁTICO
+========================== */
+
+let count = 1;
+
+document.getElementById(
+  "radio1"
+).checked = true;
+
+setInterval(() => {
+
+  nextImage();
+
+}, 5000);
+
+function nextImage(){
+
+  count++;
+
+  if(count > 4){
+
+    count = 1;
+
+  }
+
+  document.getElementById(
+    "radio" + count
+  ).checked = true;
+
+}
